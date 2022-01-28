b263969e3d89136c406c30c3128529ce2cd2bf82
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-01-28 20:39:17 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the Aiphi extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'Aiphi:plugin',
+  autoStart: true,
+  requires: [IThemeManager],
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    console.log('JupyterLab extension Aiphi is activated!');
+    const style = 'Aiphi/index.css';
+
+    manager.register({
+      name: 'Aiphi',
+      isLight: true,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  }
+};
+
+export default plugin;
